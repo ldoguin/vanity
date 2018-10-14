7dcfb6fdb319a3150df3c76854be315a73306504
Laurent Doguin <laurent.doguin@gmail.com>
2018-10-14 12:34:45 +0200

    upgrade

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const path    = require("path");
 const port = process.env.PORT || 8080;
 const firstName = process.env.FIRST_NAME || "stranger";
 const lastName = process.env.LAST_NAME || "stranger";
+const url = process.env.URL;
+app.set('view engine', 'pug')
 
-app.get('/', (req, res) => res.send('Hello !' + firstName + ' ' + lastName))
+app.get('/', (req, res) =>
+    res.render('index',{img:url, firstname:firstName, lastname: lastName, title:'hey', message : 'everything is awesome'}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
